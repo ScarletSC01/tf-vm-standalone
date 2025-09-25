c2e1cf9cc786f277896968eb66be528aa08d3496
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-09-25 09:32:49 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,7 @@ variable "zone" { default = "us-central1-a" }
 variable "credentials_file" {}
 variable "network" {}
 variable "subnetwork" {}
+variable "public_key" {
+  description = "Clave pública SSH para acceder a la VM"
+  type        = string
+}
