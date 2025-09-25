b095c834b55ddb27532cb042fec357ccf3550c1e
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-09-25 09:52:53 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,35 @@
-variable "project_id" {}
-variable "region" { default = "us-central1" }
-variable "zone" { default = "us-central1-a" }
-variable "credentials_file" {}
-variable "network" {}
-variable "subnetwork" {}
-variable "public_key" {
-  description = "Clave pública SSH para acceder a la VM"
+variable "project_id" {
+  description = "ID del proyecto en Google Cloud"
   type        = string
 }
+
+variable "region" {
+  description = "Región donde se desplegarán los recursos"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zona de la región donde se desplegará la VM"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Ruta al archivo de credenciales de servicio de GCP"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Nombre de la instancia de VM"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Tipo de máquina para la VM"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Clave pública SSH para acceso a la VM"
+  type        = string
+}
+
