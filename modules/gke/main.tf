aa5b03860683f1b2e960ce80aea0ac29b72971ac
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-09-29 10:46:46 -0300

    Update main.tf

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -1,11 +1,28 @@
-variable "project_id" {}
-variable "region" {}
-variable "cluster_name" {}
-
-resource "google_container_cluster" "gke_cluster" {
-  name     = var.cluster_name
+resource "google_container_cluster" "primary" {
+  name     = var.gke_cluster_name
   location = var.region
 
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
 }
