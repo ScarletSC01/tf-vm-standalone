035e767e0ab11c5ae6da27f300f82e82504b20cc
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-09-29 13:17:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,38 +14,34 @@ provider "google" {
   credentials = file(var.credentials_file)
 }
 
-# Módulo VM
 module "vm" {
-  source           = "./modules/vm"
-  project_id       = var.project_id
-  region           = var.region
-  zone             = var.zone
-  network          = var.network
-  subnetwork       = var.subnetwork
-  instance_name    = var.instance_name
-  machine_type     = var.machine_type
-  image            = var.image
-  public_key       = var.public_key
+  source        = "./modules/vm"
+  project_id    = var.project_id
+  region        = var.region
+  zone          = var.zone
+  instance_name = var.instance_name
+  machine_type  = var.machine_type
+  image         = var.image
+  network       = var.network
+  subnetwork    = var.subnetwork
+  public_key    = var.public_key
   credentials_file = var.credentials_file
 }
 
-# Módulo GKE
 module "gke" {
-  source           = "./modules/gke"
-  project_id       = var.project_id
-  region           = var.region
-  gke_cluster_name = var.gke_cluster_name
+  source        = "./modules/gke"
+  project_id    = var.project_id
+  region        = var.region
+  cluster_name  = "jenkins-gke-cluster"
   credentials_file = var.credentials_file
 }
 
-# Módulo CloudSQL
 module "cloudsql" {
   source           = "./modules/cloudsql"
   project_id       = var.project_id
   region           = var.region
-  cloudsql_name    = var.cloudsql_name
-  database_version = var.database_version
-  tier             = var.tier
+  cloudsql_name    = "jenkins-postgres"
+  database_version = "POSTGRES_15"
+  tier             = "db-f1-micro"
   credentials_file = var.credentials_file
 }
-
