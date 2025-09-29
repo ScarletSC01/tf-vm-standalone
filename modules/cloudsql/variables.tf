42e3c00d9bb0a4dd117b7aeeb6bd8093d84832d3
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-09-29 11:38:11 -0300

    Create variables.tf

---
+++ modules/cloudsql/variables.tf
@@ -0,0 +1,29 @@
+variable "project_id" {
+  description = "ID del proyecto en GCP"
+  type        = string
+}
+
+variable "region" {
+  description = "Región donde se desplegará CloudSQL"
+  type        = string
+}
+
+variable "cloudsql_name" {
+  description = "Nombre de la instancia CloudSQL"
+  type        = string
+}
+
+variable "database_version" {
+  description = "Versión de la base de datos"
+  type        = string
+}
+
+variable "tier" {
+  description = "Tier de la instancia"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Ruta del archivo de credenciales"
+  type        = string
+}
