164bc7b72bd0c1408f3fb08c31035f734d0e9776
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-09-29 11:53:07 -0300

    Update variables.tf

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -1,4 +1,19 @@
-variable "project_id" {}
-variable "region" {}
-variable "gke_cluster_name" {}
-variable "credentials_file" {}
+variable "project_id" {
+  description = "ID del proyecto en GCP"
+  type        = string
+}
+
+variable "region" {
+  description = "Región donde se desplegará el cluster"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Nombre del cluster GKE"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Ruta del archivo de credenciales"
+  type        = string
+}
